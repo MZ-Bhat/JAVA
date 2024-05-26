67ddf0f48d8f8196f7b98b1cbb2380c9f17d43c6
Mehran <107433204+MZ-Bhat@users.noreply.github.com>
2024-05-26 11:06:32 +0530

    Create DataSizeRange.java

---
+++ DataSizeRange.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+class DataSizeRange
+{
+public static void main(String arg[])
+
+{
+System.out.println("FLoat Min: "+Float.MIN_VALUE);
+System.out.println("Float Max: "+Float.MAX_VALUE);
+System.out.println("Float Bytes :"+Float.BYTES);
+}
+
+}
