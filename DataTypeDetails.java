39edbb75af84132d81a09ae6a717649edc009ebe
Mehran <107433204+MZ-Bhat@users.noreply.github.com>
2024-05-27 15:12:31 +0530

    Add files via upload

---
+++ DataTypeDetails.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+
+class DataTypeDetails
+{
+public static void main(String arg[])
+
+{
+int a=5;
+System.out.println(Integer.toBinaryString(a));
+
+int b=-5;
+System.out.println(Integer.toBinaryString(b));
+
+
+ 	
+
+}
+
+}
---
+++ Variables.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+
+class Variables
+{
+public static void main(String arg[])
+
+{
+char c='M';
+byte b=15;
+short s=1523;
+int i =2001;
+long l =15231523;
+float f=1.523f;
+System.out.println(c);
+System.out.println(b);
+b=23;
+System.out.println(b);
+b=127;
+System.out.println(s);
+System.out.println(i);
+System.out.println(l);
+System.out.println(f);
+}
+
+}
